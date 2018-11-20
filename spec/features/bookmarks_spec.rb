71fe580b74258d0f1d6648b775beab234fe99cf7
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-20 15:04:14 +0000

    Adding testing envinroment

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,8 +1,17 @@
-feature 'bookmarks' do
-  scenario 'user can view all bookmarks' do
+feature 'Viewing bookmarks' do
+
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
-    expect(bookmarks).to include "http://makers.tech"
-    expect(bookmarks).to include "http://www.destroyallsoftware.com"
-    expect(bookmarks).to include "http://www.google.com"
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
