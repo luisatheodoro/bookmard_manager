3381d536a6bd6734f9bc5a6c42f27dc747263209
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-20 18:44:09 +0000

    adding the possibility to add a new bookmark entry and displaying it

--- app.rb
+++ app.rb
@@ -3,12 +3,23 @@ require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
-  get '/bookmarks' do
-    @bookmark = Bookmark.all
-    erb :bookmarks
+  get '/' do
+    'Bookmark Manager'
   end
 
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :"bookmarks/index"
+  end
 
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
+  end
 
   run! if app_file == $0
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,21 +1,28 @@
 require 'bookmark'
 
 describe Bookmark do
-
   describe '.all' do
     it 'returns a list of bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
 
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include('http://www.makersacademy.com')
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com')
+    end
+  end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
     end
   end
 end
